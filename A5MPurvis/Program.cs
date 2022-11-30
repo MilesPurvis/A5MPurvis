b149196f50bea6f46d402047cf788ebb64bf1fcc
Miles Purvis <milesjsp@gmail.com>
2022-11-30 12:46:49 -0500

    Add project files.

---
+++ A5MPurvis/Program.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Program: Assignment 5 Classes.
+ * 
+ * Purpose: Records information for a student
+ * 
+ * Revision History:
+ *  Created By Miles Purvis
+ */using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5MPurvis
+{
+    internal class Program
+    {
+        //Main handles the menu
+        static void Main(string[] args)
+        {
+            //Declare variables
+            string menuInput;
+            bool menuExit = false;
+            bool studentExists = false;
+            Student myStudent = null;
+            
+            //TODO: Try Catch for null myStudent
+            do
+            {
+                Console.WriteLine("[Main Menu]\n");
+                Console.WriteLine("A) Add New Student");
+                Console.WriteLine("B) Edit Existing Student");
+                Console.WriteLine("C) Display Student");
+                Console.WriteLine("D) Exit the Program");
+                Console.Write("Select A Menu Option: ");
+                menuInput = Console.ReadLine();
+                menuInput = menuInput.ToUpper();
+
+                switch (menuInput)
+                {
+                    case "A":
+                        if (studentExists == false)
+                        {
+                            Console.Clear();
+                            studentExists = true;
+                            myStudent = AddStudent();
+                            Console.Clear();
+                           
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n[A Student record already exists]\n");
+                            Console.WriteLine("\nPress Any Key to return to menu");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        break;
+                    case "B":
+                        Console.Clear();
+                        myStudent.EditStudentInformation();
+                        Console.Clear();
+                        break;
+                    case "C":
+                        Console.Clear();
+
+                        myStudent.DisplayStudentInformation();
+                        Console.Clear();
+
+                        break;
+                    case "D":
+                        menuExit = true;
+                        break;
+                }
+            } while (menuExit == false);
+            
+            
+        }
+
+        // Take information for student
+        public static Student AddStudent()
+        {
+            Console.WriteLine("[Add Student]\n");
+            try
+            {
+                Console.Write("Enter Student ID Number: ");
+                int stuId = int.Parse(Console.ReadLine());
+
+                Console.Write("\nEnter Student Name: ");
+                string stuName = Console.ReadLine();
+
+                Console.Write("\nEnter Sutdent Age: ");
+                int stuAge = int.Parse(Console.ReadLine());
+
+                Console.Write("\nEnter Student City: ");
+                string stuCity = Console.ReadLine();
+
+                Student myStudent = new Student(stuId, stuName, stuAge, stuCity);
+
+                return myStudent;
+            }
+            catch (FormatException fEx)
+            {
+                Console.WriteLine(fEx.Message);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            return null;
+        }
+    }
+}
---
+++ A5MPurvis/Student.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5MPurvis
+{
+    internal class Student
+    {
+        private int studentId;
+        private string studentName;
+        private int studentAge;
+        private string studentCity;
+
+
+        //Constructor non-default 
+        public Student(int studentId, string studentName, int studentAge, string studentCity)
+        {
+            this.studentId = studentId;
+            this.studentName = studentName;
+            this.studentAge = studentAge;
+            this.studentCity = studentCity;
+        }
+
+
+        public Student()
+        {
+            //Constructor default
+        }
+
+        //TODO Allow user to Edit Student
+        //TODO Edit Name
+        //TODO Edit Age
+        //TODO Edit City
+        public void EditStudentInformation()
+        {
+            int studentIdSearch;
+            try
+            {
+                Console.WriteLine("[Edit Student Information]\n");
+
+                Console.Write("Enter Student ID: ");
+                studentIdSearch = int.Parse(Console.ReadLine());
+
+                if (studentIdSearch == studentId)
+                {
+                    //Allow user to edit the student
+
+                }
+                //else if the record does not exist notify the user
+                else if (studentIdSearch != studentId)
+                {
+                    Console.WriteLine("No Student Exists");
+                    Console.WriteLine("\nPress Any Key to return to menu");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+
+            }
+            catch (FormatException fEx)
+            {
+                Console.WriteLine(fEx.Message);
+                Console.WriteLine("\nPress any key to return to menu");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("\nPress any key to return to menu");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        //TODO Prompt user to re enter if invalid
+        //TODO User able to enter "QUIT" to return to menu
+        public void DisplayStudentInformation()
+        {
+            string studentSearch;
+            try
+            {
+                Console.WriteLine("[Display Student Information]\n");
+                Console.Write("Search Student by Name: ");
+                studentSearch = Console.ReadLine();
+               
+                //if record exists, Display Name|Age|City 
+                if (studentSearch == studentName)
+                {
+                    Console.WriteLine("\nName: {0}|City: {1}|Age: {2}",studentName,studentCity,studentAge);
+                    Console.WriteLine("\nPress any key to return to menu");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                //else if the record does not exist notify the user
+                else if (studentSearch != studentName)
+                {
+                    Console.WriteLine("No student record exists, Press any key to return to menu");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+            }
+            catch (FormatException fEx)
+            {
+                Console.WriteLine(fEx.Message);
+                Console.WriteLine("\nPress any key to return to menu");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("\nPress any key to return to menu");
+                Console.ReadKey();
+                Console.Clear();
+            }
+           
+        }
+
+
+
+
+
+    }
+}
